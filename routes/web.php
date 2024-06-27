a64e76af488ccab32e8df664c654d995f0be4588
Haps <thiamhapstou@gmail.com>
2024-06-27 12:40:58 +0000

    récuperation des routes

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,28 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\EvenementController;
+use App\Http\Controllers\ReservationController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+Route::resource('evenements', EvenementController::class);
+Route::get('/sidebar', function () {
+    return view('layouts/app');
+});
+//route pour la liste des evenements
+Route::get('/evenements/liste', [EvenementController::class, 'listeEvenement']);
+Route::get('/reservations/liste', [ReservationController::class, 'listeReservation']);
+
+Route::view('/', 'welcome');
+
+Route::get('dashboard',[EvenementController::class, 'listeEvenementDashboard'] )
+    ->middleware(['auth', 'verified'])
+    ->name('dashboard');
+
+Route::view('profile', 'profile')
+    ->middleware(['auth'])
+    ->name('profile');
+
+require __DIR__.'/auth.php';
