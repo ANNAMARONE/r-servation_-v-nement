15657672966a8b45a477cd1317ab321c745d2f48
Haps <thiamhapstou@gmail.com>
2024-06-27 11:44:31 +0000

    recuperer des statistique concernant un organismes dans le dashboard

--- app/Http/Controllers/ReservationController.php
+++ app/Http/Controllers/ReservationController.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
+use App\Models\Evenement;
 use App\Models\Reservation;
 use Illuminate\Http\Request;
 
@@ -14,5 +16,27 @@ class ReservationController extends Controller
         // Retourner la vue avec les données des réservations
         return view('reservations.listeReservation', compact('reservations'));
 
+    }
+
+    
+    public function statistiquesOrganismes($organismeId)
+    {
+        // Calculer le nombre total d'événements pour l'organisme spécifié
+        $totalEvenements = Evenement::where('organisme_id', $organismeId)->count();
+
+        // Calculer le nombre total de participants pour l'organisme spécifié
+        $totalParticipants = User::whereHas('reservations', function ($query) use ($organismeId) {
+            $query->whereHas('evenement', function ($query) use ($organismeId) {
+                $query->where('organisme_id', $organismeId);
+            });
+        })->count();
+
+        // Calculer le nombre total de réservations pour l'organisme spécifié
+        $totalReservations = Reservation::whereHas('evenement', function ($query) use ($organismeId) {
+            $query->where('organisme_id', $organismeId);
+        })->count();
+
+        // Retourner les statistiques à la vue
+        return view('dashboard', compact('totalEvenements', 'totalParticipants', 'totalReservations'));
     }
 }
